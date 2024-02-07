0d89e782b221e52d3e2dcfe5eeeeee2562ff84fe
Ethan Bergen <ebergen@roomandboard.com>
2024-02-07 13:32:38 -0600

    First Commit - getting project setup

---
+++ src/main/java/com/gaysinner/digitcg/DigiTcgApplication.java
@@ -0,0 +1,14 @@
+package com.gaysinner.digitcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiTcgApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DigiTcgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaysinner/digitcg/DigiTcgApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gaysinner.digitcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiTcgApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
